ed2c550008130ee0fd159937ebfa8cdcde42378d
Britton Walker <bwalker1801@gmail.com>
2017-08-26 00:18:41 -0400

    mouseenter and leave for display image.

--- js/main.js
+++ js/main.js
@@ -9,13 +9,25 @@ var color = colors[Math.floor(Math.random()*colors.length)];
 
 $( 'body' ).addClass( color );
 
-$( 'p' ).on( 'mouseover', function() {
+$( 'p' ).on( 'mouseenter', function() {
 
 	console.log($(this).attr("data-test"))
 	var url = $(this).attr("data-test")
-	// $( '.scrim' ).addClass( 'show' )
+	$(this).css('z-index', 3000);
+	$(this).css('position', 'relative');
+	$( '.scrim' ).addClass( 'show' )
 	$( '.insert' ).attr("src", url);
 	console.log( img );
 	$( 'scrim' ).append( img )
+
+})
+
+$( 'p' ).on( 'mouseleave', function() {
+
+	var url = $(this).attr("data-test")
+	$(this).css('z-index', 0);
+	$(this).css('position', 'relative');
+	$( '.scrim' ).removeClass( 'show' )
+	$( '.insert' ).attr("src", '');
 
 })
